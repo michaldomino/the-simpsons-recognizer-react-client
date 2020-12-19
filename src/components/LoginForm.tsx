d49ba20bb44f2019efe04b23cad17f4064e42cdb
Michał Domino <michal.domino@yahoo.com>
2020-12-19 13:40:18 +0100

    Add login form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import {useForm} from "react-hook-form";
+
+interface LoginData {
+    username: string
+    password: string
+}
 
 export const LoginForm: React.FC = () => {
+    const {register, handleSubmit} = useForm()
+
+    const onSubmit = (data:LoginData) => console.log(data)
+
     return (
-        <div>
-            "Hello World!"
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label>Username</label>
+            <input ref={register} name="username" />
+            <label>Password</label>
+            <input ref={register} name="password" type="password"/>
+            <button>Submit</button>
+        </form>
     )
 }
