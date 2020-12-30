d1d68f8d8e4c5b4ccb5b27ca62492d6790e2b02a
Michał Domino <michal.domino@yahoo.com>
2020-12-30 15:26:07 +0100

    Add HomeContainer, moved presentation part of Home to HomeContainer

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,6 @@
 import React, {useState} from "react";
 import {HomeContainer} from "./HomeContainer";
 import {useForm} from "react-hook-form";
-import {LoginContainer} from "../login/LoginContainer";
-import {Controls} from "../../components/controls/Controls";
-import {Button, ButtonBase, Grid, makeStyles} from "@material-ui/core";
 
 export const Home: React.FC = () => {
     const {handleSubmit} = useForm()
@@ -29,44 +26,11 @@ export const Home: React.FC = () => {
     }
 
     return (
-        <Grid container
-              spacing={2}
-              direction='column'
-        >
-
-            <Grid item>
-                <input
-                    accept="image/*"
-                    style={{display: 'none'}}
-                    id="raised-button-file"
-                    multiple
-                    type="file"
-                    onChange={handleUploadClick}
-                />
-                <label htmlFor="raised-button-file">
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        component="span"
-                    >
-                        Upload image
-                    </Button>
-                </label>
-                <label>
-                    {image?.name}
-                </label>
-            </Grid>
-
-            <Grid item>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <Controls.AppButton
-                        type='submit'
-                        disabled={image === undefined}
-                    >
-                        Submit
-                    </Controls.AppButton>
-                </form>
-            </Grid>
-        </Grid>
+        <HomeContainer
+            handleSubmit={handleSubmit}
+            onSubmit={onSubmit}
+            handleUploadClick={handleUploadClick}
+            image={image}
+        />
     )
 }
---
+++ src/pages/home/HomeContainer.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {Controls} from "../../components/controls/Controls";
+import {Button, Grid} from "@material-ui/core";
+
+interface IHomeContainerProps {
+    handleSubmit: any
+    onSubmit: () => Promise<void>
+    handleUploadClick: (event: any) => void
+    image: File | undefined
+}
+
+export const HomeContainer: React.FC<IHomeContainerProps> = (props) => {
+    const {handleSubmit, onSubmit, handleUploadClick, image} = props
+
+    return (
+        <Grid container
+              spacing={2}
+              direction='column'
+        >
+
+            <Grid item>
+                <input
+                    accept="image/*"
+                    style={{display: 'none'}}
+                    id="raised-button-file"
+                    multiple
+                    type="file"
+                    onChange={handleUploadClick}
+                />
+                <label htmlFor="raised-button-file">
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        component="span"
+                    >
+                        Upload image
+                    </Button>
+                </label>
+                <label>
+                    {image?.name}
+                </label>
+            </Grid>
+
+            <Grid item>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Controls.AppButton
+                        type='submit'
+                        disabled={image === undefined}
+                    >
+                        Submit
+                    </Controls.AppButton>
+                </form>
+            </Grid>
+        </Grid>
+    )
+}
